d94dd3056f6a2f59125400273e1aba1338564241
Baiju Abraham <baijuka@yahoo.com>
2022-12-28 17:49:05 +0000

    Add contact page

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const date = new Date();
 document.getElementById('updatedon').innerHTML=date.toDateString();
+
+function contactMsg() {
+    alert("We have received your messsage. We shall be in touch with you shortly");
+}
